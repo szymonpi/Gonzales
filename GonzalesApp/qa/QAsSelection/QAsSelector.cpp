67a5d5cedc34b5bc49481219f12b8d3e5ae28f60
Bartosz Piekny <bartosz.piekny@gmail.com>
2014-06-02 12:24:36 -0700

    new uts tests added to qasSelector

--- GonzalesApp/qa/QAsSelection/QAsSelector.cpp
+++ GonzalesApp/qa/QAsSelection/QAsSelector.cpp
@@ -8,7 +8,30 @@ QAsSelector::QAsSelector(const double &newQuestionPercent, const unsigned &maxQu
 
 }
 
-std::vector<std::shared_ptr<QA>> QAsSelector::select(std::vector<std::shared_ptr<QA>> qas)
+namespace
 {
 
+bool isOldQA(const std::shared_ptr<QA>& qa)
+{
+    return !qa->getAnswersHistory().empty();
+}
+
+}
+
+std::vector<std::shared_ptr<QA> > QAsSelector::select(std::vector<std::shared_ptr<QA> > qas)
+{
+    auto newBoundBegin = std::partition(qas.begin(), qas.end(), isOldQA);
+    int maxNew = ceil(m_maxQuestions*m_newQuestionPercent);
+    int maxOld = ceil(m_maxQuestions*(1-m_newQuestionPercent));
+
+    std::vector<std::shared_ptr<QA> > oldQAs(qas.begin(),
+                                             std::distance(qas.begin(), newBoundBegin) > maxOld ? qas.begin()+maxOld : newBoundBegin);
+
+    std::vector<std::shared_ptr<QA> > newQAs(newBoundBegin,
+                                             std::distance(newBoundBegin, qas.end()) > maxNew ? newBoundBegin+maxNew : qas.end());
+
+    oldQAs.insert(oldQAs.end(),
+                  std::make_move_iterator(newQAs.begin()),
+                  std::make_move_iterator(newQAs.end()));
+    return oldQAs;
 }
--- GonzalesUt/qa/QAsSelectorTestSuite.cpp
+++ GonzalesUt/qa/QAsSelectorTestSuite.cpp
@@ -28,6 +28,23 @@ TEST_F(QAsSelectorTestSuite, _10NewQuestionsGiven_10QuestionShouldBeSelected)
     EXPECT_EQ(10, selectedQAs.size());
 }
 
+
+TEST_F(QAsSelectorTestSuite, _2New_Max2__ShouldSelect2)
+{
+    QAsSelector selector = QAsSelector(0.60, 2);
+    std::vector<std::shared_ptr<QA>> qas{std::make_shared<QA>(), std::make_shared<QA>()};
+    auto selectedQAs = selector.select(qas);
+    EXPECT_EQ(2, selectedQAs.size());
+}
+
+TEST_F(QAsSelectorTestSuite, _2New_Max5__ShouldSelect2)
+{
+    QAsSelector selector = QAsSelector(0.60, 2);
+    std::vector<std::shared_ptr<QA>> qas{std::make_shared<QA>(), std::make_shared<QA>(), std::make_shared<QA>(), std::make_shared<QA>(), std::make_shared<QA>()};
+    auto selectedQAs = selector.select(qas);
+    EXPECT_EQ(2, selectedQAs.size());
+}
+
 TEST_F(QAsSelectorTestSuite, _11New_Max10__ShouldSelect10)
 {
     std::vector<std::shared_ptr<QA>> qas{std::make_shared<QA>(), std::make_shared<QA>(),
@@ -38,4 +55,30 @@ TEST_F(QAsSelectorTestSuite, _11New_Max10__ShouldSelect10)
                                      std::make_shared<QA>()};
     auto selectedQAs = selector.select(qas);
     EXPECT_EQ(10, selectedQAs.size());
+}
+
+bool isOldQA(const std::shared_ptr<QA>& qa)
+{
+    return !qa->getAnswersHistory().empty();
+}
+
+TEST_F(QAsSelectorTestSuite, _10New_10Old_Max10_NewFactor06__ShouldSelect6new4old)
+{
+    QDateTime dateTime(QDate::currentDate(), QTime::currentTime());
+    std::shared_ptr<QA> oldQa = std::make_shared<QA>();
+    oldQa->addHistoryEntry(dateTime, QA::AnswerRating::Correct);
+
+
+    std::shared_ptr<QA> newQa = std::make_shared<QA>();
+
+    std::vector<std::shared_ptr<QA>> qas{oldQa, oldQa, oldQa, oldQa, oldQa, oldQa, oldQa, oldQa, oldQa, oldQa,
+                                         newQa, newQa, newQa, newQa, newQa, newQa, newQa, newQa, newQa, newQa};
+
+    auto selectedQAs = selector.select(qas);
+
+    EXPECT_EQ(10, selectedQAs.size());
+    auto bound = std::partition(selectedQAs.begin(), selectedQAs.end(), isOldQA);
+    EXPECT_EQ(4, std::distance(selectedQAs.begin(), bound));
+    EXPECT_EQ(6, std::distance(bound, selectedQAs.end()));
+
 }
