5f72895bd2390052440abd2a0a15e050b849a1dc
Bartek <Bartosz Pikny>
2014-10-09 23:52:23 +0200

    minor refactoring

--- GonzalesApp/qa/QAsSelection/Selectors/QAsSelector.cpp
+++ GonzalesApp/qa/QAsSelection/Selectors/QAsSelector.cpp
@@ -9,32 +9,49 @@ public:
     }
 };
 
-
 std::vector<std::shared_ptr<QA> > QAsSelector::select(std::vector<std::shared_ptr<QA> > &qas) const
 {
     std::vector<std::shared_ptr<QA> > selected;
 
-    auto oldQAsEnd = std::partition(qas.begin(), qas.end(), isOldQA{});
+    auto oldQAsEnd = getOldQasEnd(qas.begin(), qas.end());
+    auto forRepeatEnd = getRepeatQasEnd(qas.begin(), oldQAsEnd);
+    auto notLearnedEnd = getNotLearnedEnd(forRepeatEnd, oldQAsEnd);
+    auto newQasEnd = getNewQasEnd(oldQAsEnd, qas.end());
 
-    auto forRepeatEnd = std::partition(qas.begin(), oldQAsEnd, IsForRepeatQA{{1,2,7,30,90,180,360}});
-
-    int maxForRepeat{10};
-
-    if(std::distance(qas.begin(), forRepeatEnd) > maxForRepeat)
-        forRepeatEnd = qas.begin() + maxForRepeat;
-
-    auto notLearnedEnd = std::partition(forRepeatEnd, oldQAsEnd, isNotLearned{});
     for(auto it = qas.begin(); it != notLearnedEnd; ++it)
         selected.push_back(*it);
-
-    int maxNewQAs = 10;
-
-    auto newQasEnd = qas.end();
-    if(std::distance(oldQAsEnd, qas.end())> maxNewQAs)
-        newQasEnd = oldQAsEnd + maxNewQAs;
-
     for(auto it = oldQAsEnd; it!= newQasEnd; ++it)
         selected.push_back(*it);
 
     return selected;
+}
+
+QAsSelector::QAsIter QAsSelector::getRepeatQasEnd(QAsIter begin, QAsIter end) const
+{
+    auto forRepeatEnd = std::partition(begin, end, IsForRepeatQA{periods});
+    if(std::distance(begin, forRepeatEnd) > maxForRepeat)
+        forRepeatEnd = begin + maxForRepeat;
+    return forRepeatEnd;
+}
+
+QAsSelector::QAsIter QAsSelector::getNotLearnedEnd(QAsIter begin, QAsIter end) const
+{
+    auto notLearnedEnd = std::partition(begin, end, isNotLearned{});
+    if(std::distance(begin, notLearnedEnd) > maxNotLearned)
+        notLearnedEnd = begin + maxNotLearned;
+
+    return notLearnedEnd;
+}
+
+QAsSelector::QAsIter QAsSelector::getNewQasEnd(QAsIter begin, QAsIter end) const
+{
+    auto newQasEnd = end;
+    if(std::distance(begin, end)> maxNewQAs)
+        newQasEnd = begin + maxNewQAs;
+    return end;
+}
+
+QAsSelector::QAsIter QAsSelector::getOldQasEnd(QAsSelector::QAsIter begin, QAsSelector::QAsIter end) const
+{
+    return std::partition(begin, end, isOldQA{});
 }
