40dbbfef3b066f426fb5c0874673c2ea5c252bb7
Bartosz Piekny <bartosz.piekny@gmail.com>
2014-12-21 02:28:32 -0800

    app builder script refactored

--- GonzalesUtils/binaryBuilder/libraryBuilder.py
+++ GonzalesUtils/binaryBuilder/libraryBuilder.py
@@ -1,9 +1,8 @@
-from copy import copy
+__author__ = 'Bartosz Piekny'
+
 import zipfile
-
-__author__ = 'bartek'
-
-from optparse import OptionParser
+from Printer import EmptyPrinter, Printer
+from Options import Options
 from shutil import copyfile
 from shutil import rmtree
 from zipfile import ZipFile
@@ -20,75 +19,40 @@ class AppBuilder:
                 "Qt5Gui.dll",
                 "Qt5Widgets.dll"}
 
-    def validate_options(self, options):
-        if options.platform is None:
-            print("No platform specified!")
-            exit(1)
-        if options.build_directory is None:
-            print ("No build directory specified!")
-            exit(1)
-        if options.output_directory is None:
-            print ("No output directory specified!")
-            exit(1)
-        if options.platform is "WIN":
-            if options.qt_sdk_directory is None:
-                print ("No qt sdk directory specified!")
-                exit(1)
-        if options.library_path is None:
-            print("No library path specified")
-            exit(1)
+        self.options = Options.get_options()
+        Options.validate_options(self.options)
+        if self.options.verbose is True:
+            self.printer = Printer()
+        else:
+            self.printer = EmptyPrinter()
 
-    def get_options(self):
-        parser = OptionParser()
-        parser.add_option("-p", "--platform", dest="platform",
-                          help="specify platforms", default="WIN")
-        parser.add_option("-b", "--build_directory", dest="build_directory",
-                          help="build directory", metavar="FILE")
-        parser.add_option("-o", "--output_directory", dest="output_directory",
-                          help="output directory", metavar="FILE")
-        parser.add_option("-q", "--qt_sdk_directory", dest="qt_sdk_directory",
-                          help="output directory", metavar="FILE")
-        parser.add_option("-l", "--library_path", dest="library_path",
-                  help="output directory", metavar="FILE")
-        (options, args) = parser.parse_args()
-        return options
+
 
     def app_binary_path(self, options):
-        app_binary = options.build_directory + "\\GonzalesApp\\release\\GonzalesApp.exe"
+        app_binary = options.build_directory
         return app_binary
 
     def run(self):
-        print("start...")
 
-        options = self.get_options()
-        self.validate_options(options)
+        self.printer.printStart()
+        output_binary = self.options.binary_name
+        output_binary_path = self.options.output_directory + "\\" + output_binary
+        output_zip = self.options.output_directory+"\\" + self.options.binary_name + ".zip"
+        copyfile(self.app_binary_path(self.options), output_binary_path)
 
-        print("options validated")
+        dlls_directory = self.options.qt_sdk_directory + "\\bin"
 
-        output_binary = "Gonzales.exe"
-        output_binary_path = options.output_directory + "\\" + output_binary
-        output_zip = options.output_directory+"\Gonzales.zip"
-
-
-
-        copyfile(self.app_binary_path(options), output_binary_path)
-
-        dlls_directory = options.qt_sdk_directory + "\\bin"
-        print(dlls_directory)
-
-        if options.platform is "WIN":
+        if self.options.platform is "WIN":
             for dll in self.dlls:
-                print(dlls_directory + "\\" + dll)
-                print(options.output_directory + "\\" + dll)
-                copyfile(dlls_directory + "\\" + dll, options.output_directory + "\\" + dll)
+                copyfile(dlls_directory + "\\" + dll, self.options.output_directory + "\\" + dll)
 
 
         platform_dll = "qwindows.dll"
 
         platforms_dir = "platforms"
         libraries_dir = "libraries"
-        platforms_path = options.output_directory + "\\" + platforms_dir
-        libraries_path = options.output_directory + "\\" + libraries_dir
+        platforms_path = self.options.output_directory + "\\" + platforms_dir
+        libraries_path = self.options.output_directory + "\\" + libraries_dir
 
         if not os.path.exists(platforms_path) or not os.path.isdir(platforms_path):
             os.mkdir(platforms_path)
@@ -96,26 +60,26 @@ class AppBuilder:
         if not os.path.exists(libraries_path) or not os.path.isdir(libraries_path):
             os.mkdir(libraries_path)
 
-        copyfile(options.qt_sdk_directory + "\\plugins\\platforms\\" + platform_dll, platforms_path + "\\" + platform_dll)
-        libraries = os.listdir(options.library_path)
+        copyfile(self.options.qt_sdk_directory + "\\plugins\\platforms\\" + platform_dll, platforms_path + "\\" + platform_dll)
+        libraries = os.listdir(self.options.library_path)
 
         for library in libraries:
-            copyfile(options.library_path + "\\" + library, libraries_path + "\\" + library)
+            copyfile(self.options.library_path + "\\" + library, libraries_path + "\\" + library)
 
-        print("[APP RELEASE BUILDER] --- All files copied")
+        self.printer.printCopyStatus()
 
         with ZipFile(output_zip, 'w', zipfile.ZIP_DEFLATED) as myzip:
             myzip.write(output_binary_path, output_binary)
 
             for dll in self.dlls:
-                myzip.write(options.output_directory + "\\" + dll, dll)
+                myzip.write(self.options.output_directory + "\\" + dll, dll)
 
             myzip.write(platforms_path + "\\" + platform_dll, platforms_dir + "\\" + platform_dll)
 
             for library in libraries:
                 myzip.write(libraries_path + "\\" + library, libraries_dir + "\\" + library)
 
-        print("[APP RELEASE BUILDER] --- All files zipped")
+        self.printer.printZipStatus()
 
         to_delete_files = self.dlls
         to_delete_files.add(output_binary)
@@ -123,14 +87,10 @@ class AppBuilder:
         rmtree(platforms_path)
         rmtree(libraries_path)
         for file in to_delete_files:
-            os.remove(options.output_directory + "\\" + file)
+            os.remove(self.options.output_directory + "\\" + file)
 
-        print("[APP RELEASE BUILDER] --- Successfully clean-up")
-        print("[APP RELEASE BUILDER] --- zip path : ")
-        print(output_zip)
-
-
-
+        self.printer.printCleanUpStatus()
+        self.printer.printEndStatus(output_zip)
 
 
 def main():
