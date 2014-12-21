000fff63b12bf77bdce2ba4d62f5dc02de7eae9d
Bartosz Piekny <bartosz.piekny@gmail.com>
2014-12-21 06:44:34 -0800

    build script refactoring

--- GonzalesUtils/binaryBuilder/libraryBuilder.py
+++ GonzalesUtils/binaryBuilder/libraryBuilder.py
@@ -32,62 +32,83 @@ class AppBuilder:
         app_binary = options.build_directory
         return app_binary
 
+    def output_binary_path(self):
+        return  self.options.output_directory + "\\" + self.options.binary_name + ".exe"
+
+    def dlls_directory(self):
+        return self.options.qt_sdk_directory + "\\bin"
+
+    def create_subfolders(self, output_libraries_path, output_platforms_path):
+        if not os.path.exists(output_platforms_path) or not os.path.isdir(output_platforms_path):
+            os.mkdir(output_platforms_path)
+        if not os.path.exists(output_libraries_path) or not os.path.isdir(output_libraries_path):
+            os.mkdir(output_libraries_path)
+
+    def libraries_to_copy(self, output_libraries_path):
+        files_to_copy = dict()
+        libraries = os.listdir(self.options.library_path)
+        for library in libraries:
+            files_to_copy[self.options.library_path + "\\" + library] = output_libraries_path + "\\" + library
+        return files_to_copy
+
+    def dlls_to_copy(self, dlls_directory, files_to_copy):
+        files_to_copy = dict()
+        if self.options.platform is "WIN":
+            for dll in self.dlls:
+                files_to_copy[dlls_directory + "\\" + dll] = self.options.output_directory + "\\" + dll
+        return files_to_copy
+
     def run(self):
 
         self.printer.printStart()
-        output_binary = self.options.binary_name
-        output_binary_path = self.options.output_directory + "\\" + output_binary
+
         output_zip = self.options.output_directory+"\\" + self.options.binary_name + ".zip"
-        copyfile(self.app_binary_path(self.options), output_binary_path)
-
-        dlls_directory = self.options.qt_sdk_directory + "\\bin"
-
-        if self.options.platform is "WIN":
-            for dll in self.dlls:
-                copyfile(dlls_directory + "\\" + dll, self.options.output_directory + "\\" + dll)
-
-
+        output_binary_path = self.output_binary_path()
+        dlls_directory = self.dlls_directory()
         platform_dll = "qwindows.dll"
 
         platforms_dir = "platforms"
         libraries_dir = "libraries"
-        platforms_path = self.options.output_directory + "\\" + platforms_dir
-        libraries_path = self.options.output_directory + "\\" + libraries_dir
+        output_platforms_path = self.options.output_directory + "\\" + platforms_dir
+        output_libraries_path = self.options.output_directory + "\\" + libraries_dir
+        input_platform_dll_path = self.options.qt_sdk_directory + "\\plugins\\platforms\\" + platform_dll
+        output_platform_dll_path = output_platforms_path + "\\" + platform_dll
 
-        if not os.path.exists(platforms_path) or not os.path.isdir(platforms_path):
-            os.mkdir(platforms_path)
+        self.create_subfolders(output_libraries_path, output_platforms_path)
 
-        if not os.path.exists(libraries_path) or not os.path.isdir(libraries_path):
-            os.mkdir(libraries_path)
-
-        copyfile(self.options.qt_sdk_directory + "\\plugins\\platforms\\" + platform_dll, platforms_path + "\\" + platform_dll)
-        libraries = os.listdir(self.options.library_path)
-
-        for library in libraries:
-            copyfile(self.options.library_path + "\\" + library, libraries_path + "\\" + library)
+        files_to_copy = dict()
+        files_to_copy[self.app_binary_path(self.options)] = output_binary_path
+        files_to_copy.update(self.dlls_to_copy(dlls_directory, files_to_copy))
+        files_to_copy[input_platform_dll_path] = output_platform_dll_path
+        files_to_copy.update(self.libraries_to_copy(output_libraries_path))
 
         self.printer.printCopyStatus()
 
+        for in_path, out_path in files_to_copy.iteritems():
+            copyfile(in_path, out_path)
+
         with ZipFile(output_zip, 'w', zipfile.ZIP_DEFLATED) as myzip:
-            myzip.write(output_binary_path, output_binary)
-
-            for dll in self.dlls:
-                myzip.write(self.options.output_directory + "\\" + dll, dll)
-
-            myzip.write(platforms_path + "\\" + platform_dll, platforms_dir + "\\" + platform_dll)
-
-            for library in libraries:
-                myzip.write(libraries_path + "\\" + library, libraries_dir + "\\" + library)
+            for file_to_zip in files_to_copy.values():
+                output_target = file_to_zip.replace(self.options.output_directory, "");
+                myzip.write(file_to_zip, output_target)
+            # myzip.write(output_binary_path, self.options.binary_name)
+            #
+            # for dll in self.dlls:
+            #     myzip.write(self.options.output_directory + "\\" + dll, dll)
+            #
+            # myzip.write(output_platforms_path + "\\" + platform_dll, platforms_dir + "\\" + platform_dll)
+            #
+            # for library in libraries:
+            #     myzip.write(output_libraries_path + "\\" + library, libraries_dir + "\\" + library)
 
         self.printer.printZipStatus()
 
-        to_delete_files = self.dlls
-        to_delete_files.add(output_binary)
 
-        rmtree(platforms_path)
-        rmtree(libraries_path)
-        for file in to_delete_files:
-            os.remove(self.options.output_directory + "\\" + file)
+        for value in files_to_copy.itervalues():
+            os.remove(value)
+
+        rmtree(output_platforms_path)
+        rmtree(output_libraries_path)
 
         self.printer.printCleanUpStatus()
         self.printer.printEndStatus(output_zip)
